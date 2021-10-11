51f97b29a3f4f54ef5ebfcba5c13e0d9fef36f0a
denis-png <maxheimerd@outlook.com>
2021-10-11 11:51:39 +0200

    Global remaster of the project. python_api -> python_scripts, changed structure. django -> web, now settings use .env, corrected migrations, using legacy db data.

--- python_api/tests/temp.py
+++
@@ -1,4 +0,0 @@
-a = '0011'
-b = int(a)
-print(str(b))
-print(len(str(b)))
--- query/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import *
-from .dash_apps.finished import plot
-
-urlpatterns = [
-    path('select/', Select.as_view(), name='select'),
-    path('save/', save_data, name='save'),
-]
